6c44c41604b87f34eaf4679c4e4ef870216cfaa0
Romain Bossart <romain.bossart@fastmail.com>
2016-11-05 08:34:21 +0100

    2. convert input data ta output values with Linear Scales

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-document.body.innerHTML = `<h1>${d3.version}</h1>`;
+var linearScale = d3.scaleLinear()
+  .domain([0, 100])
+  .range([0, 600])
+  .clamp(true);
+
+console.log(linearScale(-20));
+console.log(linearScale(50));
+console.log(linearScale(100));
+console.log(linearScale(105));
+
+console.log(linearScale.invert(300)); // range -> domain
