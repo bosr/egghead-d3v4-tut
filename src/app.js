343cff21ecabaf470915ebaf3cc1e456ef5c1418
Romain Bossart <romain.bossart@fastmail.com>
2016-11-05 19:41:01 +0100

    16. Create Chart Axes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 // Margin convention for D3
-var margin = { top: 30, right: 20, bottom: 0, left: 50 };
+var margin = { top: 20, right: 10, bottom: 60, left: 40 };
 var width = 425 - margin.left - margin.right;
 var height = 625 - margin.top - margin.bottom;
 
@@ -22,3 +22,31 @@ svg.append('rect')
   .attr('height', height)
   .style('fill', 'lightblue')
   .style('stroke', 'green');
+
+var yScale = d3.scaleLinear()
+  .domain([0, 100])
+  .range([height, 0]);  // in SVG, y coordinate is top - bottom
+
+var yAxis = d3.axisLeft(yScale).ticks(5);  // ticks is a hint
+// .ticks(5, '%') for percentages (needs domain [0, 1])
+// .ticks(5, '.2s') 2 decimals
+// .ticks(5, 's') 10M
+// .ticksValues([8, 19, 43, 77])
+svg.call(yAxis); // sufficient to draw an axis
+
+var xScale = d3.scaleTime()
+  .domain([new Date(2016, 0, 1), new Date(2016, 1, 1)])
+  .range([0, width]);
+
+var xAxis = d3.axisBottom(xScale)
+  .ticks(3)
+  // .ticks(d3.timeMinute.every(45))
+  .tickSize(20);
+  // .tickSizeInner(10)
+  // .tickSizeOuter(20)
+  // .tickPadding(15)
+
+svg
+  .append('g')
+    .attr('transform', `translate(0, ${height})`)
+  .call(xAxis);
