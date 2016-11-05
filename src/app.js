76395e7c0e6104b7ba66bf9187c9f18654c0716a
Romain Bossart <romain.bossart@fastmail.com>
2016-11-05 12:12:17 +0100

    8. Modify DOM Elements

--- src/app.js
+++ src/app.js
@@ -1,26 +1,19 @@
-var div = d3.select('div');
-console.log(div.nodes());
-
-var a = d3.select('a');
-console.log(a.nodes());
-var as = d3.selectAll('a');
-console.log(as.nodes());
-
-var divLinks = div.selectAll('a');
-console.log(divLinks.nodes());
-
-// using a css selector (or anything that is accepted by document.query.selector)
-console.log(d3.selectAll('div a').nodes());
-
-
-var actionLink = d3.select('.action');
-console.log(actionLink.nodes());
-
 var secondLink = d3.selectAll('a:nth-child(2)');
 console.log(secondLink.nodes());
 
+// read attribute
+console.log(secondLink.attr('href'));
 
-// turn a native list of nodes into a D3 Selection
-var allLinks = d3.selectAll(document.links);
-console.log(allLinks.nodes());
-console.log(allLinks.size());
+// create or update attribute
+secondLink.attr('href', 'http://www.google.com');
+secondLink.style('color', 'green');
+console.log(secondLink.attr('href'));
+
+// more idiomatic
+d3.selectAll('a:nth-child(3)')
+  .attr('href', 'http://www.bing.com')
+  // .style('color', 'pink')
+  .classed('red', true)
+  // .text('Inventory')
+  .html('Inventory <b>SALE</b>')
+  ;
