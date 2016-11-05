064f9cbf760e67c7dd131bab1ab959ec2be00836
Romain Bossart <romain.bossart@fastmail.com>
2016-11-05 18:53:02 +0100

    15. Margin Convention

--- src/app.js
+++ src/app.js
@@ -1,58 +1,24 @@
-var scores = [
-  { name: 'Alice', score: 96 },
-  { name: 'Billy', score: 83 },
-  { name: 'Cindy', score: 91 },
-  { name: 'David', score: 96 },
-  { name: 'Emily', score: 88 }
-];
+// Margin convention for D3
+var margin = { top: 30, right: 20, bottom: 0, left: 50 };
+var width = 425 - margin.left - margin.right;
+var height = 625 - margin.top - margin.bottom;
 
-// 'g' are SVG analogs of 'div'
-
-// bar is now a selection of abstract 'g' elements
-var bar = d3.select('.chart')
+var svg = d3.select('.chart')
   .append('svg')
-    .attr('width', 225)  // svg elements have a default size
-    .attr('height', 300)
-  .selectAll('g')
-  .data(scores)
-  .enter()
-    .append('g')
-    .attr('transform', (d, i) => 'translate(0, ' + i * 33 + ')');
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-function scaleBar (selection, scale) {
-  selection.style('transform', 'scaleX(' + scale + ')');
-}
+svg.append('rect')
+  .attr('width', width / 2)
+  .attr('height', height)
+  .style('fill', 'lightblue')
+  .style('stroke', 'green');
 
-function fade (selection, opacity) {
-  selection
-    .filter(':not(:hover)')
-    .style('fill-opacity', opacity);
-}
-
-function setColor (selection, color) {
-  selection.style('fill', color);
-}
-
-// append a rect to each 'g' element of 'bar' selection
-bar.append('rect')
-  .style('width', d => d.score)
-  .attr('class', 'bar')
-  .on('mouseover', function (d, i, elements) { // all native elements of bar
-    d3.select(this).call(scaleBar, 2);
-    d3.selectAll(elements)
-      .call(setColor, 'teal')
-      .call(fade, 0.5);
-  })
-  .on('mouseout', function (d, i, elements) { // 'd' is the data
-    d3.select(this).call(scaleBar, 1);
-    d3.selectAll(elements)
-      .call(setColor, 'lightgreen')
-      .call(fade, 1);
-  });
-
-// append a text element (with offset y) to each 'g' element of 'bar' selection
-bar.append('text')
-  .attr('y', 20)
-  .text(function (d) {
-    return d.name;
-  });
+svg.append('rect')
+  .attr('x', width / 2)
+  .attr('width', width / 2)
+  .attr('height', height)
+  .style('fill', 'lightblue')
+  .style('stroke', 'green');
