aaf10cdaa0b3566aa31e97117ebbfb594daad10e
Romain Bossart <romain.bossart@fastmail.com>
2016-11-05 21:34:20 +0100

    18. Build a Column Chart

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 // https://sarasoueidan.com/tags/svg/index.html
 //
 // Margin convention for D3
-var margin = { top: 20, right: 10, bottom: 30, left: 40 };
+var margin = { top: 20, right: 10, bottom: 60, left: 40 };
 var width = 400 - margin.left - margin.right;
 var height = 600 - margin.top - margin.bottom;
 
@@ -13,18 +13,18 @@ var svg = d3.select('.chart')
   .append('g')
     .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-svg.append('rect')
-  .attr('width', width / 2)
-  .attr('height', height)
-  .style('fill', 'lightblue')
-  .style('stroke', 'green');
 
-svg.append('rect')
-  .attr('x', width / 2)
-  .attr('width', width / 2)
-  .attr('height', height)
-  .style('fill', 'lightblue')
-  .style('stroke', 'green');
+var data = [
+  { score: 63, subject: 'Mathematics' },
+  { score: 82, subject: 'Geography' },
+  { score: 74, subject: 'Spelling' },
+  { score: 97, subject: 'Reading' },
+  { score: 52, subject: 'Science' },
+  { score: 74, subject: 'Chemistry' },
+  { score: 97, subject: 'Physics' },
+  { score: 52, subject: 'ASL' }
+]
+
 
 var yScale = d3.scaleLinear()
   .domain([0, 100])
@@ -33,8 +33,11 @@ var yScale = d3.scaleLinear()
 var yAxis = d3.axisLeft(yScale).ticks(5);  // ticks is a hint
 svg.call(yAxis); // sufficient to draw an axis
 
-var xScale = d3.scaleTime()
-  .domain([new Date(2016, 0, 1), new Date(2016, 1, 1)])
+var xScale = d3.scaleBand()
+  .padding(0.2)
+  // .paddingInner(0.2)
+  // .paddingOuter(0.3)
+  .domain(data.map(d => d.subject))
   .range([0, width]);
 
 var xAxis = d3.axisBottom(xScale)
@@ -43,7 +46,20 @@ var xAxis = d3.axisBottom(xScale)
 svg
   .append('g')
     .attr('transform', `translate(0, ${height})`)
-  .call(xAxis);
+  .call(xAxis)
+  .selectAll('text')
+  .style('text-anchor', 'end')
+  .attr('transform', 'rotate(-45)');
+
+svg.selectAll('rect')
+  .data(data)
+  .enter()
+  .append('rect')
+    .attr('x', d => xScale(d.subject))
+    .attr('y', d => yScale(d.score))
+    .attr('width', d => xScale.bandwidth())
+    .attr('height', d => height - yScale(d.score))
+    // .style('fill', 'steelblue');
 
 function responsivefy(svg) {
   // get container + svg aspect ratio
