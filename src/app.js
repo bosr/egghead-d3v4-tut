7a5d3b67012096053f4fd34a1466c63073046a9a
Romain Bossart <romain.bossart@fastmail.com>
2016-11-05 09:16:29 +0100

    6. Load and inspect Data

--- src/app.js
+++ src/app.js
@@ -1,7 +1,38 @@
-var ordinalScale = d3.scaleOrdinal()
-  .domain(["poor", "good", "great"])
-  .range(["red", "white", "green"]);
+// Loading data
+//
+// d3.json('data/data.json', function (data) {
+//   console.log(data);
+// });
 
-console.log(ordinalScale("good"));
-console.log(ordinalScale("great"));
-console.log(ordinalScale("poor"));
+// d3.csv('data/data.csv', function (data) {
+//   console.log(data);
+// });
+
+// d3.tsv('data/data.tsv', function (data) {
+//   console.log(data);
+// });
+
+d3.json('data/data.json', function (data) {
+  var min = d3.min(data, function (d) {
+    return d.age;
+  });
+  console.log(min);
+
+  var extent = d3.extent(data, function (d) {
+    return d.age;
+  });
+  console.log(extent);
+
+  var scale = d3.scaleLinear()
+    .domain(extent)
+    .range([0, 600]);
+  console.log(scale(24));
+  console.log(scale(38));
+
+  // getting a set from the values
+  var ages = d3.set(data, function (d) {
+    return d.age;
+  });
+  console.log(ages);
+  console.log(ages.values());
+});
