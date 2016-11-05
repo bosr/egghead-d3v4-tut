1d8c13c266063dc74fe4cdb24202456710039189
Romain Bossart <romain.bossart@fastmail.com>
2016-11-05 08:51:53 +0100

    3. Convert dates to numeric values with Time Scales

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-var linearScale = d3.scaleLinear()
-  .domain([0, 100])
-  .range([0, 600])
+var timeScale = d3.scaleTime()
+  .domain([new Date(2016, 1, 1), new Date(2016, 12, 31)])
+  .range([0, 100])
   .clamp(true);
 
-console.log(linearScale(-20));
-console.log(linearScale(50));
-console.log(linearScale(100));
-console.log(linearScale(105));
+console.log(timeScale(new Date(2015, 12, 30)));
+console.log(timeScale(new Date(2016, 1, 15)));
+console.log(timeScale(new Date(2016, 7, 1)));
+console.log(timeScale(new Date(2016, 12, 31)));
+console.log(timeScale(new Date()));
 
-console.log(linearScale.invert(300)); // range -> domain
+console.log(timeScale.invert(50));
