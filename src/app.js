a32d3970ba67bc1fd049f925eab3f7455318ae52
Romain Bossart <romain.bossart@fastmail.com>
2016-11-05 23:02:27 +0100

    25. Animate with the General Update Pattern part 1

--- src/app.js
+++ src/app.js
@@ -1,31 +1,72 @@
-// warning: d3.transition() starts the transition immediately
-// maybe we need to include it in a function call
+var data = [
+  { name: 'Alice', score: 37 },
+  { name: 'Billy', score: 69 },
+  { name: 'Cindy', score: 86 },
+  { name: 'David', score: 44 },
+  { name: 'Emily', score: 59 }
+];
 
-function go () {
-  var t = d3.transition()
-    .delay(1000)
-    .duration(1000);
+var margin = { top: 10, right: 10, bottom: 30, left: 30 };
+var width = 400 - margin.left - margin.right;
+var height = 565 - margin.top - margin.bottom;
 
-  d3.selectAll('.block')
-    .transition(t)
-    .style('width', '400px');
+var svg = d3.select('.chart')
+  .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .call(responsivefy)
+  .append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-  d3.select('.a')
-    .transition(t)
-    .style('background-color', 'orange');
+var xScale = d3.scaleBand()
+  .domain(data.map(d => d.name))
+  .range([0, width])
+  .padding(0.2);
+svg
+  .append('g')
+    .attr('transform', `translate(0, ${height})`)
+  .call(d3.axisBottom(xScale));
 
-  d3.select('.b')
-    .transition(t)
-    .style('background-color', 'blue');
-}
+var yScale = d3.scaleLinear()
+  .domain([0, 100])
+  .range([height, 0]);
+svg
+  .append('g')
+  .call(d3.axisLeft(yScale));
 
-function configure (t, delay, duration) {
-  return t.delay(delay).duration(duration);
-}
+svg
+  .selectAll('rect')
+  .data(data)
+  .enter()
+  .append('rect')
+  .attr('x', d => xScale(d.name))
+  .attr('y', d => yScale(d.score))
+  .attr('width', d => xScale.bandwidth())
+  .attr('height', d => height - yScale(d.score));
 
-function goNow () {
-  d3.selectAll('.block')
-    .transition()
-    .call(configure, 1000, 1000)
-    .style('width', '300px');
+function responsivefy(svg) {
+  // get container + svg aspect ratio
+  var container = d3.select(svg.node().parentNode),
+    width = parseInt(svg.style('width')),
+    height = parseInt(svg.style('height')),
+    aspect = width / height;
+
+  // add viewBox and preserveAspectRatio properties,
+  // and call resize so that svg resizez on initial page load
+  svg.attr('viewBox', `0 0 ${width} ${height}`)
+    .attr("preserveAspectRatio", "xMinYMid")
+    .call(resize);
+
+  // to register multiple listeners for same event type,
+  // you need to add namespace, i.e., 'click.foo'
+  // necessary if you call invoke this function for multiple svgs
+  // api docs: https://github.com/mbostock/d3/wiki/Selections#on
+  d3.select(window).on("resize." + container.attr("id"), resize);
+
+  // get width of container and resize svg to fit it
+  function resize() {
+    var targetWidth = parseInt(container.style("width"));
+    svg.attr('width', targetWidth);
+    svg.attr('height', Math.round(targetWidth / aspect));
+  }
 }
